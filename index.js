5064dac301a4ffa6b6948b28decd999049a9308e
11134212 <chilikenewbekov@gmail.com>
2022-09-28 18:17:40 +0300

    homework

---
+++ index.js
@@ -0,0 +1,161 @@
+// первое задание
+// for (let i = 1; i < 6; i++ ) {
+//     console.log(i)
+// }
+
+// второе задание
+// const arr = [2, 3, 4, 5]
+// let sum = 1;
+// for (let i = 0; i < arr.length; i++ ){
+//     sum = sum * arr[i]
+//     console.log(sum);
+// }
+
+// // третье задание
+// const obj = {
+// 	'Минск': 'Беларусь',
+// 	'Москва': 'Россия',
+// 	'Киев': 'Украина'
+// };
+// for (const capital in obj){
+//     console.log('Минск' + '- это'  + ' Беларусь')
+// }
+
+// четвертое задание
+// for(i = 11; i < 34; i++ ){
+//     console.table(i)
+// }
+
+// пятое задание
+// const obj = {
+//     '200':'Коля',
+//     '300':'Вася',
+//     '400':'Петя',
+// }
+// for (const salary in obj){
+//     console.log(salary + ' - зарплата ' + obj[salary])
+// }
+
+// Шестое задание
+// for(let n = 1000; 50 < n; n++ ){
+//      n /= 2
+//     console.log(n);
+// }
+
+// седьмое задание
+// const num = 10
+// if(num === 10){
+//     console.log('Верно');
+// } else{
+//     console.log('Неверно')
+// }
+
+// Восьмое задание
+// let min =15
+//     if( min <= 15){
+//         console.log('Первая четверть')
+//     }
+//     if(min > 15 & min <= 30){
+//         console.log('Вторая четверть')
+//     }
+//     if(min > 30 & min <= 45){
+//         console.log('Третья четверть')
+//     }
+//     if(min > 45 & min <= 59){
+//         console.log('Четвертая четверть')
+//     }
+
+// Девятое задание
+// Переменная lang может принимать 2 значения: 'ru' 'en'.
+// Если она имеет значение 'ru', то в переменную arr запишем массив дней недели на русском языке,
+// а если имеет значение 'en' – то на английском. 	
+// Решите задачу через 2 if, через switch-case и через многомерный массив 	без ифов и switch. 
+
+// let lang = prompt('Введите ru или en')
+
+// while (lang !== 'ru' && lang !== 'en'){
+//     lang = prompt('Введите ru или en')
+// }
+
+// if(lang == 'ru'){
+//     console.log('пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс');
+// }
+// if(lang == 'en'){
+//     console.log('mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn')
+// }
+
+// switch(lang){
+//     case 'ru': arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+//     console.log(arr)
+//     break
+//     case 'en': arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+//     console.log(arr)
+//     break
+// }
+
+
+
+
+// Десятое задание
+// let a = 0
+// if( a === 0){
+//     console.log('Верно')
+// } else{
+//     console.log('Неверно')
+// }
+
+// Одиннадцатое задание
+// let a = 1
+// if( 0 < a){
+//     console.log('Верно')
+// } else{
+//     console.log('Неверно')
+// }
+
+// Двенадцатое задание
+// let a = 2
+// if( a < 0){
+//     console.log('Верно')
+// } else{
+//     console.log('Неверно')
+// }
+
+// Тринадцатое задание
+// let a = 3
+// if( 0 <= a){
+//     console.log('Верно')
+// } else{
+//     console.log('Неверно')
+// }
+
+// Четырнадцатое задание
+// let a = 0
+// if( a <= 0){
+//     console.log('Верно')
+// } else{
+//     console.log('Неверно')
+// }
+
+// Пятнадцатое задание
+// let a = 1
+// if( a != 0){
+//     console.log('Верно')
+// } else{
+//     console.log('Неверно')
+// }
+
+// Шестнадцатое задание
+// let a = 'test'
+// if( a === 'test'){
+//     console.log('Верно')
+// } else{
+//     console.log('Неверно');
+// }
+
+// Семнадцатое задание
+// let a = 1
+// if(a === 1){
+//     console.log('Верно');
+// } else{
+//     console.log('Неверно');
+// }
